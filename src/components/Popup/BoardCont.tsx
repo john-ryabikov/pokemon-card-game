3db09d67924b889d7dfeb227b13c1b9e903b9ccc
john-ryabikov <zhenya.ryabikov@mail.ru>
2024-10-11 11:33:39 +0300

    v.0.0.63

--- src/components/Popup/BoardCont.tsx
+++ src/components/Popup/BoardCont.tsx
@@ -14,45 +14,47 @@ export default function BoardCont() {
             transition={{ delay: 0.15 }}
         >
             <p className='popup__title popup__title_small'>Правила игры!</p>
-            <div className='popup__text-wrapper'>
-                <div className='popup__text-wrap'>
-                    <p className='popup__text'>
-                        Твоя задача - победить вражеского Покемона.<br/><br/>
+            <div className='popup__text-wrapper-box'>
+                <div className='popup__text-wrapper'>
+                    <div className='popup__text-wrap'>
+                        <p className='popup__text'>
+                            Твоя задача - победить вражеского Покемона.<br/><br/>
 
-                        Добывай карточку энергии и ищи подходящую<br/>
-                        для своего игрового Покемона. В каждой новой<br/> 
-                        дуэли карточки будут выпадать<br/>
-                        в <b>случайном</b> порядке.<br/><br/>
+                            Добывай карточку энергии и ищи подходящую<br/>
+                            для своего игрового Покемона. В каждой новой<br/> 
+                            дуэли карточки будут выпадать<br/>
+                            в <b>случайном</b> порядке.<br/><br/>
 
-                        Как только ты полностью заполнишь энергию<br/>
-                        своего Покемона, то <b>обязательно</b> должен<br/>
-                        атаковать вражеского Покемона.<br/><br/>
+                            Как только ты полностью заполнишь энергию<br/>
+                            своего Покемона, то <b>обязательно</b> должен<br/>
+                            атаковать вражеского Покемона.<br/><br/>
 
-                        Каждый раз, когда ты добываешь энергию,<br/>
-                        у вражеского Покемона заряжается стамина.
-                    </p>
-                    <img className='popup__indicate-enemy' src="img/Icons/turn-stack_icon.png" alt="Indicator" draggable="false"/>
-                    <p className='popup__text'>
-                        Если она будет полностью заряжена, то<br/>
-                        следующая добыча карточки даст вражескому<br/>
-                        Покемону энергию, и как только он наберет нужное<br/>
-                        количество энергии, то <b>немедленно атакует</b><br/> 
-                        твоего Покемона.<br/><br/> 
+                            Каждый раз, когда ты добываешь энергию,<br/>
+                            у вражеского Покемона заряжается стамина.
+                        </p>
+                        <img className='popup__indicate-enemy' src="img/Icons/turn-stack_icon.png" alt="Indicator" draggable="false"/>
+                        <p className='popup__text'>
+                            Если она будет полностью заряжена, то<br/>
+                            следующая добыча карточки даст вражескому<br/>
+                            Покемону энергию, и как только он наберет нужное<br/>
+                            количество энергии, то <b>немедленно атакует</b><br/> 
+                            твоего Покемона.<br/><br/> 
 
-                        Каждый удар дает количество <b>ПокеКоинов</b> <img src="img/Icons/pokecoin_icon.svg" alt="PokeCoin" draggable="false"/><br/>      
-                        равное силе атаки твоего Покемона.<br/><br/>
+                            Каждый удар дает количество <b>ПокеКоинов</b> <img src="img/Icons/pokecoin_icon.svg" alt="PokeCoin" draggable="false"/><br/>      
+                            равное силе атаки твоего Покемона.<br/><br/>
 
-                        За побежденного вражеского<br/> 
-                        Покемона начисляются <b>ПокеКоины</b> <img src="img/Icons/pokecoin_icon.svg" alt="PokeCoin" draggable="false"/><br/>       
-                        равные здоровью противника.<br/><br/> 
+                            За побежденного вражеского<br/> 
+                            Покемона начисляются <b>ПокеКоины</b> <img src="img/Icons/pokecoin_icon.svg" alt="PokeCoin" draggable="false"/><br/>       
+                            равные здоровью противника.<br/><br/> 
 
-                        Так же они начисляются дополнительно<br/> 
-                        за уровень сложности.<br/><br/>
+                            Так же они начисляются дополнительно<br/> 
+                            за уровень сложности.<br/><br/>
 
-                        Уровень сложности можно выбрать в<br/>
-                        разделе настроек <img src="img/Icons/settings_icon.svg" alt="Setting" draggable="false"/>  
-                    </p>
-                    <p className='popup__title popup__title_small'>Желаем удачи!</p>
+                            Уровень сложности можно выбрать в<br/>
+                            разделе настроек <img src="img/Icons/settings_icon.svg" alt="Setting" draggable="false"/>  
+                        </p>
+                        <p className='popup__title popup__title_small'>Желаем удачи!</p>
+                    </div>
                 </div>
             </div>
             <Button 
