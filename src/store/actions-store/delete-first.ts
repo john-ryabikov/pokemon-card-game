cdccfc5694d87573391a8409b643ef16e800005b
john-ryabikov <zhenya.ryabikov@mail.ru>
2024-10-18 15:40:15 +0300

    v.0.0.82

--- src/store/actions-store/delete-first.ts
+++ src/store/actions-store/delete-first.ts
@@ -7,7 +7,7 @@ export const deleteFirstAction = (
 
     switch (firsted) {
         case 'store-closed':
-            set({isFirstOpenStore: false})
+            set({isFirstOpenStore: null})
         break;
         case 'board-closed':
             set({isFirstOpenBoard: null})
