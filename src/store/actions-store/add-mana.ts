6f40a94164321862f6478a08b5a6af6d08fe2a74
john-ryabikov <zhenya.ryabikov@mail.ru>
2024-10-19 15:08:47 +0300

    v.0.0.88-2

--- src/store/actions-store/add-mana.ts
+++ src/store/actions-store/add-mana.ts
@@ -18,85 +18,265 @@ export const addManaAction = (
 
     const diff = (nowDate.getTime() - state.lastTime) / (1000 * 60)
 
+    const manaCont = (mana: number) => {
+        switch (mana) {
+            case 9:
+                set({ mana: state.mana +=1 });
+            break;
+            case 8:
+                set({ mana: state.mana +=1 });
+            break;
+            case 7:
+                set({ mana: state.mana +=1 });
+            break;
+            case 6:
+                set({ mana: state.mana +=1 });
+            break;
+            case 5:
+                set({ mana: state.mana +=1 });
+            break;
+            case 4:
+                set({ mana: state.mana +=1 });
+            break;
+            case 3:
+                set({ mana: state.mana +=1 });
+            break;
+            case 2:
+                set({ mana: state.mana +=1 });
+            break;
+        }
+    }
+
+    // Проходит меньше 10 минут
+
     if (diff < 10) set({
-        mana: 0,
         timer: setInterval(() => {
             set({ mana: state.mana += 1 })
             if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
         }, 60000 * 10)
     })
-    if (diff >= 10 && diff < 20) set({
-        mana: 1,
-        lastTime: nowDate.getTime(),
-        timer: setInterval(() => {
-            set({ mana: state.mana += 1 })
-            if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
-        }, 60000 * 10)
-    })
-    if (diff >= 20 && diff < 30) set({
-        mana: 2,
-        lastTime: nowDate.getTime(),
-        timer: setInterval(() => {
-            set({ mana: state.mana += 1 })
-            if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
-        }, 60000 * 10)
-    })
-    if (diff >= 30 && diff < 40) set({
-        mana: 3,
-        lastTime: nowDate.getTime(),
-        timer: setInterval(() => {
-            set({ mana: state.mana += 1 })
-            if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
-        }, 60000 * 10)
-    })
-    if (diff >= 40 && diff < 50) set({
-        mana: 4,
-        lastTime: nowDate.getTime(),
-        timer: setInterval(() => {
-            set({ mana: state.mana += 1 })
-            if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
-        }, 60000 * 10)
-    })
-    if (diff >= 50 && diff < 60) set({
-        mana: 5,
-        lastTime: nowDate.getTime(),
-        timer: setInterval(() => {
-            set({ mana: state.mana += 1 })
-            if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
-        }, 60000 * 10)
-    })
-    if (diff >= 60 && diff < 70) set({
-        mana: 6,
-        lastTime: nowDate.getTime(),
-        timer: setInterval(() => {
-            set({ mana: state.mana += 1 })
-            if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
-        }, 60000 * 10)
-    })
-    if (diff >= 70 && diff < 80) set({
-        mana: 7,
-        lastTime: nowDate.getTime(),
-        timer: setInterval(() => {
-            set({ mana: state.mana += 1 })
-            if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
-        }, 60000 * 10)
-    })
-    if (diff >= 80 && diff < 90) set({
-        mana: 8,
-        lastTime: nowDate.getTime(),
-        timer: setInterval(() => {
-            set({ mana: state.mana += 1 })
-            if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
-        }, 60000 * 10)
-    })
-    if (diff >= 90 && diff < 100) set({
-        mana: 9,
-        lastTime: nowDate.getTime(),
-        timer: setInterval(() => {
-            set({ mana: state.mana += 1 })
-            if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
-        }, 60000 * 10)
-    })
+
+    // Проходит больше 10 минут
+
+    if (diff >= 10 && diff < 20) {
+        if (state.mana + 1 === state.maxMana) {
+            set({
+                mana: state.mana += 1,
+                lastTime: 0,
+                timer: null
+            })
+        } else set({
+            mana: state.mana += 1,
+            lastTime: nowDate.getTime(),
+            timer: setInterval(() => {
+                set({ mana: state.mana += 1 })
+                if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
+            }, 60000 * 10)
+        })
+    }
+
+    // Проходит больше 20 минут
+
+    if (diff >= 20 && diff < 30) {
+        if (state.mana + 2 > state.maxMana) {
+            set({
+                mana: state.mana += 1,
+                lastTime: 0,
+                timer: null
+            })
+        } else if (state.mana + 2 === state.maxMana) {
+            set({
+                mana: state.mana += 2,
+                lastTime: 0,
+                timer: null
+            })
+        } else set({
+            mana: state.mana += 2,
+            lastTime: nowDate.getTime(),
+            timer: setInterval(() => {
+                set({ mana: state.mana += 1 })
+                if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
+            }, 60000 * 10)
+        })
+    }
+    
+    // Проходит больше 30 минут
+
+    if (diff >= 30 && diff < 40) {
+        if (state.mana + 3 > state.maxMana) {
+            manaCont(state.mana)
+            set({
+                lastTime: 0,
+                timer: null
+            })
+        } else if (state.mana + 3 === state.maxMana) {
+            set({
+                mana: state.mana += 3,
+                lastTime: 0,
+                timer: null
+            })
+        } else set({
+            mana: state.mana += 3,
+            lastTime: nowDate.getTime(),
+            timer: setInterval(() => {
+                set({ mana: state.mana += 1 })
+                if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
+            }, 60000 * 10)
+        })
+    }
+    
+    // Проходит больше 40 минут
+
+    if (diff >= 40 && diff < 50) {
+        if (state.mana + 4 > state.maxMana) {
+            manaCont(state.mana)
+            set({
+                lastTime: 0,
+                timer: null
+            })
+        } else if (state.mana + 4 === state.maxMana) {
+            set({
+                mana: state.mana += 4,
+                lastTime: 0,
+                timer: null
+            })
+        } else set({
+            mana: state.mana += 4,
+            lastTime: nowDate.getTime(),
+            timer: setInterval(() => {
+                set({ mana: state.mana += 1 })
+                if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
+            }, 60000 * 10)
+        })
+    }
+
+    // Проходит больше 50 минут
+
+    if (diff >= 50 && diff < 60) {
+        if (state.mana + 5 > state.maxMana) {
+            manaCont(state.mana)
+            set({
+                lastTime: 0,
+                timer: null
+            })
+        } else if (state.mana + 5 === state.maxMana) {
+            set({
+                mana: state.mana += 5,
+                lastTime: 0,
+                timer: null
+            })
+        } else set({
+            mana: state.mana += 5,
+            lastTime: nowDate.getTime(),
+            timer: setInterval(() => {
+                set({ mana: state.mana += 1 })
+                if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
+            }, 60000 * 10)
+        })
+    }
+
+    // Проходит больше 60 минут
+
+    if (diff >= 50 && diff < 60) {
+        if (state.mana + 5 > state.maxMana) {
+            manaCont(state.mana)
+            set({
+                lastTime: 0,
+                timer: null
+            })
+        } else if (state.mana + 5 === state.maxMana) {
+            set({
+                mana: state.mana += 5,
+                lastTime: 0,
+                timer: null
+            })
+        } else set({
+            mana: state.mana += 5,
+            lastTime: nowDate.getTime(),
+            timer: setInterval(() => {
+                set({ mana: state.mana += 1 })
+                if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
+            }, 60000 * 10)
+        })
+    }
+
+    // Проходит больше 70 минут
+
+    if (diff >= 50 && diff < 60) {
+        if (state.mana + 5 > state.maxMana) {
+            manaCont(state.mana)
+            set({
+                lastTime: 0,
+                timer: null
+            })
+        } else if (state.mana + 5 === state.maxMana) {
+            set({
+                mana: state.mana += 5,
+                lastTime: 0,
+                timer: null
+            })
+        } else set({
+            mana: state.mana += 5,
+            lastTime: nowDate.getTime(),
+            timer: setInterval(() => {
+                set({ mana: state.mana += 1 })
+                if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
+            }, 60000 * 10)
+        })
+    }
+
+    // Проходит больше 80 минут
+
+    if (diff >= 50 && diff < 60) {
+        if (state.mana + 5 > state.maxMana) {
+            manaCont(state.mana)
+            set({
+                lastTime: 0,
+                timer: null
+            })
+        } else if (state.mana + 5 === state.maxMana) {
+            set({
+                mana: state.mana += 5,
+                lastTime: 0,
+                timer: null
+            })
+        } else set({
+            mana: state.mana += 5,
+            lastTime: nowDate.getTime(),
+            timer: setInterval(() => {
+                set({ mana: state.mana += 1 })
+                if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
+            }, 60000 * 10)
+        })
+    }
+
+    // Проходит больше 90 минут
+
+    if (diff >= 50 && diff < 60) {
+        if (state.mana + 5 > state.maxMana) {
+            manaCont(state.mana)
+            set({
+                lastTime: 0,
+                timer: null
+            })
+        } else if (state.mana + 5 === state.maxMana) {
+            set({
+                mana: state.mana += 5,
+                lastTime: 0,
+                timer: null
+            })
+        } else set({
+            mana: state.mana += 5,
+            lastTime: nowDate.getTime(),
+            timer: setInterval(() => {
+                set({ mana: state.mana += 1 })
+                if(state.mana >= state.maxMana) clearInterval(get().timer as NodeJS.Timeout)
+            }, 60000 * 10)
+        })
+    }
+
+    // Проходит больше 100 минут
+
     if (diff > 100) set({
         mana: 10,
         lastTime: 0,
